bdb511e4693eba8a6381397595c7f166b8002578
hanselhansel <49292620+hanselhansel@users.noreply.github.com>
2026-02-22 12:47:20 +0800

    feat: wire 6 new agent-readiness checks into checks package
    Re-export check_agents_md, check_markdown_accept, check_mcp_endpoint,
    check_semantic_html, check_x402, check_nlweb from checks/__init__.py.

--- src/context_cli/core/checks/__init__.py
+++ src/context_cli/core/checks/__init__.py
@@ -1,21 +1,33 @@
 """Per-pillar check functions for readiness linting."""
 
+from context_cli.core.checks.agents_md import check_agents_md
 from context_cli.core.checks.content import check_content
 from context_cli.core.checks.content_usage import check_content_usage
 from context_cli.core.checks.eeat import check_eeat
 from context_cli.core.checks.llms_txt import check_llms_txt
+from context_cli.core.checks.markdown_accept import check_markdown_accept
+from context_cli.core.checks.mcp_endpoint import check_mcp_endpoint
+from context_cli.core.checks.nlweb import check_nlweb
 from context_cli.core.checks.robots import AI_BOTS, DEFAULT_TIMEOUT, check_robots
 from context_cli.core.checks.rsl import check_rsl
 from context_cli.core.checks.schema import check_schema_org
+from context_cli.core.checks.semantic_html import check_semantic_html
+from context_cli.core.checks.x402 import check_x402
 
 __all__ = [
     "AI_BOTS",
     "DEFAULT_TIMEOUT",
+    "check_agents_md",
     "check_content",
     "check_content_usage",
     "check_eeat",
     "check_llms_txt",
+    "check_markdown_accept",
+    "check_mcp_endpoint",
+    "check_nlweb",
     "check_robots",
     "check_rsl",
     "check_schema_org",
+    "check_semantic_html",
+    "check_x402",
 ]
